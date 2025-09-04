bc25fbe4587aac55baef40dc8603fb535983eb1a
Thiago Henrique Dos Santos <thiaghenrique145@gmail.com>
2025-09-04 13:26:16 -0300

    #1 e #2 leet code

---
+++ LeetCode/1. Two Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < nums.Length; i++)
+        {
+            for (j = i + 1; j < nums.Length; j++)
+            {
+                if ((nums[i] + nums[j]) == target)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(j);
+                    return [i, j];
+                }
+            }
+        }
+        return [i, j];
+    }
+
+    public static void Main(String[] args)
+    {
+        int[] nums = { 2, 5, 5, 11 };
+        int target = 10;
+        TwoSum(nums, target);
+    }
+}
---
+++ LeetCode/Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+public partial class Program
+{
+    public static bool IsPalindrome(int x)
+    {
+        int Invertido = 0;
+
+        if (x < 0)
+        {
+            return false;
+        }
+
+        while (x != 0)
+        {
+            Invertido = Invertido * 10 + x % 10;
+            x /= 10;
+        }
+        Console.WriteLine($"{x},{Invertido}");
+        if (x != Invertido)
+        {
+            return false;
+        }
+
+        else if (x == Invertido)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void Main(String[] args)
+    {
+        int x = 10;
+        IsPalindrome(x);
+    }
+}
